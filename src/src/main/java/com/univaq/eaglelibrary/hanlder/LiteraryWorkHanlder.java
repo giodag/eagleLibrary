d7cb911789bcb247d0d3e1814e41e9f2d5fab70d
Sabato Sodano <sabato.sodano@alliance-healthcare.net>
2019-06-20 15:03:15 +0200

    insert some controller implementation

--- src/src/main/java/com/univaq/eaglelibrary/hanlder/LiteraryWorkHanlder.java
+++ src/src/main/java/com/univaq/eaglelibrary/hanlder/LiteraryWorkHanlder.java
@@ -7,6 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.univaq.eaglelibrary.controller.LiteraryWork;
+import com.univaq.eaglelibrary.dto.LiteraryWorkDTO;
+import com.univaq.eaglelibrary.dto.LiteraryWorkListDTO;
+import com.univaq.eaglelibrary.dto.LiteraryWorkListFilterDTO;
+import com.univaq.eaglelibrary.dto.ResultDTO;
 import com.univaq.eaglelibrary.persistence.PersistenceService;
 import com.univaq.eaglelibrary.persistence.exceptions.DatabaseException;
 
@@ -30,7 +34,33 @@ public class LiteraryWorkHanlder implements LiteraryWork {
 		}
 		
 		return result;
+	}
+
+	public LiteraryWorkDTO getLiteraryWork(LiteraryWorkDTO literaryWorkDTO) {
+		Set<Map<String, String>> result = null;
+		try {
+			// test per andare a fare la lettura a secco, qua ovviamente devono essere passati i filtri
+			result = persistenceService.search("user", "first_name = 'sab'");
+		} catch (DatabaseException e) {
+			logger.error("LiteraryWorkHanlder : Error in search literaryWork. StackTrace : ",e);
+		}
 		
+		return null;
+	}
+
+	public LiteraryWorkDTO getLiteraryWorkTranscribed(LiteraryWorkDTO literaryWorkDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public LiteraryWorkListDTO getLiteraryWork(LiteraryWorkListFilterDTO literaryWorkListFilterDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResultDTO createUpdateLiteraryWork(LiteraryWorkDTO literaryWorkDTO) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/src/main/java/com/univaq/eaglelibrary/utility/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author ssodano
- *
- */
-package com.univaq.eaglelibrary.utility;
