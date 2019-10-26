6efab16e44fbc6046b5217dcd757abdbe8f94059
Sabato Sodano <s.sodano@ponyu.it>
2019-10-26 13:04:28 +0200

    Fix query

--- src/src/main/java/com/univaq/eaglelibrary/repository/UserRepository.java
+++ src/src/main/java/com/univaq/eaglelibrary/repository/UserRepository.java
@@ -4,18 +4,19 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.univaq.eaglelibrary.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	@Query(value = "SELECT * FROM USER u WHERE u.activated = 1", nativeQuery = true)
+	@Query("select u from User u WHERE u.activated = 1")
 	public List<User> findAllActiveUsers();
 	
 	public User findByUsername(String username);
 
-	@Query("select u from FROM USER u inner join u.user_transcription ut"
-		    + "where u.activated = true "
+	@Query("select u from User u "
+		    + "WHERE u.activated = 1 "
 		    + "and (coalesce(:idsUser, null) is null or u.id in (:idsUser)) ")
-	public List<User> findUsersByFilter(List<Long> ids);
+	public List<User> findUsersByFilter( @Param("idsUser") List<Long> idsUser);
 }
